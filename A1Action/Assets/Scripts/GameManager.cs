3d99d7594fc46303d1b1e94287f48a2b8478d15c
emrevtnsvr <e.vatansever885@mybvc.ca>
2025-05-15 13:25:21 -0700

    FirstWeekAssignmentChallenge
    I created a GameManager script and worked on GameStates and UI transitions.

---
+++ A1Action/Assets/Scripts/GameManager.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance;
+
+    public GameState currentState = GameState.Menu;
+
+    public enum GameState
+    {
+        Menu,
+        LevelSelection
+    }
+
+    private void Awake()
+    {
+        // Eğer başka bir GameManager varsa bunu yok et
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject); // Sahne geçişlerinde kalıcı olsun
+        }
+        else
+        {
+            Destroy(gameObject);
+           
+        }
+    }
+
+    private void Start()
+    {
+        SetGameState(GameState.Menu);
+    }
+
+    public void SetGameState(GameState newState)
+    {
+        if (currentState == newState)
+        {
+            currentState = newState;
+        }
+        switch (currentState)
+        {
+            case GameState.Menu:
+                Debug.Log("MainMenu");
+            break;
+            case GameState.LevelSelection:
+                Debug.Log("LevelSelection");
+            break;
+        }
+    }
+
+    public void LevelSelectionScene()
+    {
+  
+        SceneManager.LoadScene("LevelSelection");
+        
+    }
+
+    public void ChooseLevelOne()
+    {
+
+        SceneManager.LoadScene("Level1");
+    }
+
+    public void ChooseLevelTwo()
+    {
+
+        SceneManager.LoadScene("Level2");
+    }
+}
